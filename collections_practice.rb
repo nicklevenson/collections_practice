a3f8505ecbb760c104c412ce16d1a640ed2d583d
Nicholas Levenson <nicklevenson@gmail.com>
2020-10-22 10:10:19 -0700

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,57 @@
 
+def sort_array_asc(array)
+    array.sort
+end
+
+def sort_array_desc(array)
+    array.sort do |b, a|
+        a<=>b
+    end
+end
+
+def sort_array_char_count(array)
+    array.sort do |a,b|
+        a.length<=>b.length
+    end
+end
+
+def swap_elements(array)
+   array[1], array[2] = array[2], array[1]
+   array
+end
+
+def reverse_array(array)
+    array.reverse
+end
+
+def kesha_maker(array) 
+    array.each do |word|
+        word[2] = "$";
+    end
+end
+
+def find_a(array)
+    array.select do |word|
+        word[0] == "a"
+    end
+end
+
+def sum_array(array)
+    sum = 0;
+    array.each do |int|
+        sum = sum + int;
+    end
+    sum
+end
+
+def add_s(array)
+    newArray = []
+    array.each do |word|
+        if array.index(word) != 1
+            newArray.push(word + "s")
+        else
+            newArray.push(word);
+        end
+    end
+    newArray
+end
